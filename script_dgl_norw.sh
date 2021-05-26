32556ada8b0288350527476f664298b60d0c1b99
hoangdzung <hoangdzung@github.com>
2021-05-27 04:07:33 +0700

    add script

--- script_dgl_norw.sh
+++ script_dgl_norw.sh
@@ -5,4 +5,4 @@ time python3 ./tools/launch.py \
 --num_servers 1 \
 --part_config $1_$2/$1.json \
 --ip_config ip_config.txt \
-"python3 train_dist_unsupervised.py --graph_name $1 --ip_config ip_config.txt --num_servers 1 --num_workers 12 --num_epochs 1 --batch_size 2000 --num_gpus -1"
+"python3 train_dist_unsupervised.py --graph_name $1 --ip_config ip_config.txt --num_servers 1 --num_workers 12 --num_epochs 1 --batch_size 2000 --num_gpus -1 --random_walk"
